742ac832b33176b0f37b8e9dbfa2543a2297902f
luciana orozco <lucianaorozco2001@gmail.com>
2026-05-30 13:08:50 -0500

    segundo ejercicios funciones4

---
+++ ejerciciod_funciones4.py
@@ -0,0 +1,9 @@
+def crear_tarjeta(nombre, mensaje="¡Felicidades!", ocasion="cumpleaños"):
+    print("************************")
+    print(f"Para: {nombre}")
+    print(f"Ocasión: {ocasion}")
+    print(mensaje)
+    print("************************\n")
+
+crear_tarjeta("Ana")
+crear_tarjeta("Carlos", "¡Muchos éxitos!", "graduación")
